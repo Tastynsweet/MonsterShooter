85db6e75c2af6ac1cd48f9d22fe8d610afe5c135
Andy <ac744303@gmail.com>
2025-11-27 21:07:49 -0800

    Button Implementation
    Finished implementing buttons. All buttons in the main menu now work properly

--- My Project/Assets/Scripts/MainMenu/MainMenu.cs
+++ My Project/Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     const string GAME_SCENE = "Game Scene";
+
+    [SerializeField] private GameObject menuPop;
+    [SerializeField] private GameObject creditPop;
+
+    [SerializeField] private GameObject backgroundDim;
+
+    private GameObject activePanel;
+    
+
+    private void Update()
+    {
+        if (activePanel != null && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ClosePanel();
+        }
+    }
     public void playGame()
     {
         SceneManager.LoadScene(GAME_SCENE, LoadSceneMode.Single);
@@ -15,5 +32,45 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Game Quit");
         Application.Quit();
+    }
+
+    public void OpenControls()
+    {
+        OpenPanel(menuPop);
+    }
+    
+    public void OpenCredits()
+    {
+        OpenPanel(creditPop);
+    }
+
+    private void OpenPanel(GameObject panel)
+    {
+        if (activePanel != null)
+        {
+            activePanel.SetActive(false);
+        }
+
+        panel.SetActive(true);
+        activePanel = panel;
+
+        if (backgroundDim != null)
+        {
+            backgroundDim.SetActive(true);
+        }
+    }
+
+    public void ClosePanel()
+    {
+        if (activePanel != null)
+        {
+            activePanel.SetActive(false);
+            activePanel = null;
+        }
+
+        if (backgroundDim != null)
+        {   
+            backgroundDim.SetActive(false);
+        }
     }
 }
